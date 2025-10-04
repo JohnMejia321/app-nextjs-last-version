93263d9f86127105bc86cd5f892fd41e20ebdf14
JohnMejia321 <john.mejias@udea.edu.co>
2025-10-03 21:23:39 -0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
           },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+            value: 'GET,POST,PUT,DELETE,OPTIONS'
           },
           {
             key: 'Access-Control-Allow-Headers',
@@ -31,40 +31,8 @@ const nextConfig = {
           }
         ]
       }
-    ]
-  }
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true'
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Cookie'
-          },
-          {
-            key: 'Access-Control-Expose-Headers',
-            value: 'Set-Cookie'
-          },
-          {
-            key: 'Set-Cookie',
-            value: '_vercel_sso_nonce=; Path=/; Secure; SameSite=None; HttpOnly'
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,POST,PUT,DELETE,OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
     ];
-  },
+  }
 };
 
 export default nextConfig;
